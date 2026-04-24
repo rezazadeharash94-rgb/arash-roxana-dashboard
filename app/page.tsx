51e0f260058644d6287ad2255be88e82968ca6d1
rezazadeharash94-rgb <rezazadeh.arash94@gmail.com>
2026-04-24 13:30:02 +0330

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 import { FinanceChart } from "@/components/FinanceChart";
 import { GoalsPanel } from "@/components/GoalsPanel";
 import { ChecklistPanel } from "@/components/ChecklistPanel";
